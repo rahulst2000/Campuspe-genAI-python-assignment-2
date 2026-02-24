284ad901076609aec17317d6e3a48fe62386a66d
Rahul <rjrahulreddy@gmail.com>
2026-02-24 14:01:03 +0530

    personal bio card

---
+++ personal_bio_card.py
@@ -0,0 +1,15 @@
+# This program collects basic personal details from the user.
+# The user enters name, age, email, college name, and city.
+# The program displays the entered information.
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+Email = input("Enter your email: ")
+College_name = input("Enter your college name: ")
+City = input("Enter your city: ")
+
+print("Name: ", name)
+print("Age: ", age)
+print("Email: ", Email)
+print("College: ", College_name)
+print("City: ", City)
